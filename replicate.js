7d51021476b472a24c8f052532ca13a13dcf37c7
nmcasasr <38595223+nmcasasr@users.noreply.github.com>
2019-12-18 21:41:20 -0500

    Update replicate.js

--- replicate.js
+++ replicate.js
@@ -1,7 +1,8 @@
 rs.initiate( {
   _id : "rs0",
   members: [
-    { _id: 0, host: `parchapp_event_db:27017` },
-    { _id: 1, host: `parchapp_group_db:27017` }
+    { _id: 0, host: `parchapp_event_db1:27017` },
+    { _id: 1, host: `parchapp_event_db2:27017` },
+    { _id: 2, host: `parchapp_event_db3:27017` }
   ]
 });
